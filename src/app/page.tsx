8647073ad07ef95467308cb4738f915bac9c4b20
EucTech <euccreative@gmail.com>
2025-04-10 05:59:19 -0400

    updated home page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import GetMatched from "@/components/HomePage/GetMatched";
 import HeroSection from "@/components/HomePage/HeroSection";
+import LetTalk from "@/components/LetTalk";
 import Steps from "@/components/HomePage/Steps";
 
 
@@ -7,6 +9,8 @@ export default function Home() {
     <div>
       <HeroSection/>
       <Steps/>
+      <GetMatched/>
+      <LetTalk/>
     </div>
   );
 }
