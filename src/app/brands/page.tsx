722841a7c0c41bdf4b4181fbebcd460dd1723538
EucTech <euccreative@gmail.com>
2025-04-10 15:47:02 -0400

    updated

--- src/app/brands/page.tsx
+++ src/app/brands/page.tsx
@@ -1,8 +1,21 @@
+import BrandHeroSection from '@/components/BrandsPage/BrandHeroSection'
+import DashboardSection from '@/components/BrandsPage/DashboardSection'
+import GetStarted from '@/components/BrandsPage/GetStarted'
+import InfoCard from '@/components/BrandsPage/InfoCard'
+import ServicesCard from '@/components/BrandsPage/ServicesCard'
+import SimpleStep from '@/components/BrandsPage/SimpleStep'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>Brands</div>
+    <div>
+      <BrandHeroSection/>
+      <DashboardSection/>
+      <InfoCard/>
+      <SimpleStep/>
+      <ServicesCard/>
+      <GetStarted/>
+    </div>
   )
 }
 
