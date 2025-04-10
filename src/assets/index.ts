ae42404cbf6904240628f12a6bc54f560320c2c4
EucTech <euccreative@gmail.com>
2025-04-10 15:45:58 -0400

    updated

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -6,6 +6,9 @@ import spark from "./images/spark.png";
 import brown_star from "./images/b-star.png";
 import icon1 from "./images/icon1.png";
 import getmatch from "./images/getmatch.png";
+import dashbaord from "./images/dashboard.png";
+import step1 from "./images/step1.png";
+import service from "./images/service.png";
 
 
 export const Images = { 
@@ -17,6 +20,10 @@ export const Images = {
     brown_star,
     icon1,
     getmatch,
+    dashbaord,
+    step1,
+    service,
+    
 
 
 }
